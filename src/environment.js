42aed3dac9252a370de6a78cc99e04e711cbedc4
RJorge09 <rafael.jorge09@gmail.com>
2019-03-19 12:11:35 -0300

    test key2

---
+++ src/environment.js
@@ -0,0 +1,14 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyC4kTPrxZN_067pNivxEouLrlQVV_hSssA",
+    authDomain: "opcaodev-1233.firebaseapp.com",
+    databaseURL: "https://opcaodev-1233.firebaseio.com",
+    projectId: "opcaodev-1233",
+    storageBucket: "opcaodev-1233.appspot.com",
+    messagingSenderId: "32255598931"
+};
+
+export const TESTE = {
+    nome: "teste"
+};
+
+//export default TESTE;
